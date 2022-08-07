d8f874b9b46662058f76de232e19ca0c15a3e9f3
AG977 <93376305+AG977@users.noreply.github.com>
2022-08-07 16:11:42 +0200

    Added task 4.4

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations  } from './api';
+import axios from 'axios';
+import './nprogress.css';
+
 
 
 class App extends Component {
