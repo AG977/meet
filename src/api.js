d38b4c6f3a0ac80ccdc460a7e4853697ee0a8429
AG977 <93376305+AG977@users.noreply.github.com>
2022-08-17 19:23:53 +0200

    update

--- src/api.js
+++ src/api.js
@@ -91,7 +91,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
-    'https://8scveetdph.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + '/' + encodeCode
+    'https://8scveetdph.execute-api.eu-central-1.amazonaws.com/dev/api/token/'+ encodeCode
   )
     .then((res) => {
       return res.json();
