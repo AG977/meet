e6123c3cd15678b92803e48eb41d2bec5d67e7b1
AG977 <93376305+AG977@users.noreply.github.com>
2022-12-17 21:01:39 +0100

    welcome screen

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations  } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken  } from './api';
 import './nprogress.css';
 import { OfflineAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
 
 
 
@@ -18,6 +19,7 @@ class App extends Component {
     locations: [],
     currentLocation: "All",
     numberOfEvents: 32,
+    showWelcomeScreen: undefined
   };
 
   componentDidMount() {
@@ -28,6 +30,23 @@ class App extends Component {
         }
       });
     }
+
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+    true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+        }
+      }); 
+    }
+  } 
 
     componentWillUnmount(){
       this.mounted = false;
@@ -56,12 +75,17 @@ updateNumberOfEvents = (numberOfEvents) => {
 
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div
+    className="App" />
+
     return (
       <div className="App">
       <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
       <EventList events={this.state.events} />
       <NumberOfEvents updateNumberOfEvents={this.updateNumberOfEvents} updateEvents={() => this.updateEvents()} />
       {!navigator.onLine && <OfflineAlert text={'You are currently offline, data may be not updated.'}/>}
+      <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+        getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
